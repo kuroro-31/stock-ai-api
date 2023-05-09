dd161af087ffcf0ab4ddedad1018a8df95a1a726
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-05-09 13:47:28 +0900

    修正

--- atoms/東証一部上場企業銘柄コード取得.py
+++ atoms/東証一部上場企業銘柄コード取得.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 # スクレイピングで東証一部上場企業銘柄コードを取得する
 url = "https://ja.wikipedia.org/wiki/%E6%9D%B1%E4%BA%AC%E8%A8%BC%E5%88%B8%E5%8F%96%E5%BC%95%E6%89%80%E3%83%97%E3%83%A9%E3%82%A4%E3%83%A0%E5%B8%82%E5%A0%B4%E4%B8%8A%E5%A0%B4%E4%BC%81%E6%A5%AD%E4%B8%80%E8%A6%A7"
 
