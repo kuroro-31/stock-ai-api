b180737286f9a32339efa34a0f2b1e57e02631a3
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-05-13 10:18:50 +0900

    sbiにログインまでを追加

---
+++ components/sbi/login.py
@@ -0,0 +1,29 @@
+# components/sbi_login.py
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from components.config import SBI_USERNAME, SBI_PASSWORD
+
+
+def login_sbi(driver):
+    driver.get('https://www.sbisec.co.jp/ETGate')
+
+    username_value = SBI_USERNAME
+    password_value = SBI_PASSWORD
+
+    # ユーザーネームを入力
+    username = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "user_id"))
+    )
+    username.send_keys(username_value)
+
+    # パスワードを入力
+    password = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "user_password"))
+    )
+    password.send_keys(password_value)
+
+    ACTlogin = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "ACT_login"))
+    )
+    ACTlogin.click()
--- index.py
+++ index.py
@@ -1,31 +1,12 @@
 from components.webdriver_setup import setup_driver
-from components.config import APP_ENV, SBI_USERNAME, SBI_PASSWORD
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from components.sbi.login import login_sbi
+from components.config import APP_ENV
 
 
 def main():
     driver = setup_driver()
-    print(type(driver))
 
-    # ブラウザの操作を行う
-    driver.get('https://www.sbisec.co.jp/ETGate')
-
-    username_value = SBI_USERNAME
-    password_value = SBI_PASSWORD
-
-    # ユーザーネームを入力
-    username = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "user_id"))
-    )
-    username.send_keys(username_value)
-
-    # パスワードを入力
-    password = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "user_password"))
-    )
-    password.send_keys(password_value)
+    login_sbi(driver)  # SBI証券にログイン
 
     # 開発中でない場合は、ドライバを終了する
     if APP_ENV != 'local':
